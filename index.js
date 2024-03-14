7bd68c273689e54a1b16501ccea73097064b0b55
Vatsal Pipalava <vatsalpipalava@gmail.com>
2024-03-14 20:23:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require("./database/connection");
 app.use(require("./router/auth"));
 app.use(require("./router/cards"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
